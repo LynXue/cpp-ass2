11e50f14a26a0d7f0b69e61033a6745b360cb163
z5462028 <z5462028@vx09.orchestra.cse.unsw.EDU.AU>
2024-07-07 20:01:36 +1000

    continue add some tests

--- src/filtered_string_view.test.cpp
+++ src/filtered_string_view.test.cpp
@@ -28,3 +28,52 @@ TEST_CASE("Constructor with std::string and predicate") {
 	REQUIRE(sv[1] == 'e');
 	REQUIRE(sv[2] == 'o');
 }
+
+TEST_CASE("Constructor with C-string") {
+	const char* str = "hello";
+	fsv::filtered_string_view sv(str);
+	REQUIRE(sv.size() == std::strlen(str));
+	REQUIRE(!sv.empty());
+	REQUIRE(sv.data() == str);
+}
+
+TEST_CASE("Constructor with C-string and predicate") {
+	const char* str = "hello world";
+	auto predicate = [](const char& c) { return c != ' '; };
+	fsv::filtered_string_view sv(str, predicate);
+	REQUIRE(sv.size() == 10);
+	REQUIRE(!sv.empty());
+	REQUIRE(sv[0] == 'h');
+	REQUIRE(sv[1] == 'e');
+	REQUIRE(sv[2] == 'l');
+	REQUIRE(sv[3] == 'l');
+	REQUIRE(sv[4] == 'o');
+}
+
+TEST_CASE("Copy constructor") {
+	std::string str = "hello world";
+	fsv::filtered_string_view sv1(str);
+	fsv::filtered_string_view sv2 = sv1;
+	REQUIRE(sv1.data() == sv2.data());
+	REQUIRE(sv1.size() == sv2.size());
+}
+
+TEST_CASE("Move constructor") {
+	std::string str = "move";
+	fsv::filtered_string_view sv1(str);
+	fsv::filtered_string_view sv2(std::move(sv1));
+	REQUIRE(sv2.size() == str.size());
+	REQUIRE(sv2.data() == str.data());
+	REQUIRE(sv1.size() == 0);
+	REQUIRE(sv1.data() == nullptr);
+}
+
+TEST_CASE("Assignment operator") {
+	std::string str1 = "assign1";
+	std::string str2 = "assign2";
+	fsv::filtered_string_view sv1(str1);
+	fsv::filtered_string_view sv2(str2);
+	sv1 = sv2;
+	REQUIRE(sv1.size() == sv2.size());
+	REQUIRE(sv1.data() == sv2.data());
+}
